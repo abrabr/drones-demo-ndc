bfd218612a26b5173d9245744de1571d6990982a
Anders Breivik <abr@knowit.no>
2016-01-12 01:00:13 +0100

    Run wheelOn

--- swarm-wheels-on.js
+++ swarm-wheels-on.js
@@ -6,7 +6,8 @@ Cylon.robot({
 
     connections: {
         'rolling-spider-swarm': { adaptor: 'rolling-spider-swarm', uuid:'daa2f6fd67ee44f6bb1f78e698e0f170,b0cbcc5a717343f19e9fe92dc397acb9'}, //RS_R033811 Rød 1
-        //'rolling-spider': { adaptor: 'rolling-spider', uuid:'b0cbcc5a717343f19e9fe92dc397acb9'}, //RS_R032641
+        //'rolling-spider': { adaptor: 'rolling-spider', uuid:'b0cbcc5a717343f19e9fe92dc397acb9'}, //RS_R032641 Rød 2
+        //'rolling-spider': { adaptor: 'rolling-spider', uuid:'3d465913364849138f181bce99673f51'}, //RS_B020918 Blå
         'keyboard': { adaptor: 'keyboard' }
     },
 
@@ -17,19 +18,21 @@ Cylon.robot({
 
     work: function (my) {
 
-        // my.drone.wheelOn();
-        // my.drone.flatTrim();
-
-        my.drone.takeOff();
+        my.swarm.wheelOn();
+        my.swarm.flatTrim();
 
         my.keyboard.on('up', function(key) { 
-            my.drone.takeOff();
+            my.swarm.takeOff();
+            my.swarm.hover();
         });
 
         my.keyboard.on('down', function(key) { 
-            my.drone.land();
+            my.swarm.land();
         });
 
+        my.keyboard.on('f', function(key) { 
+            my.swarm.frontFlip();
+        });
         
     }
 
